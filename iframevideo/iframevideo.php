4d79a4f900f97b6ffee7edc9873a19d0d3a27c90
Luis Hernandez <lahr0809@gmail.com>
2024-05-17 09:28:56 -0500

    Add comment use shortcode Video

--- iframevideo/iframevideo.php
+++ iframevideo/iframevideo.php
@@ -12,4 +12,5 @@ add_shortcode( "yvideo", function($atts, $content){
     $id = $atts['id'];
     return '<div class="responsiveContent"><iframe width="560" height="315" src="https://www.youtube.com/embed/'.$id.'?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
    });
+   //[yvideo id="ImOR0o-QHOQ"]
 ?>
